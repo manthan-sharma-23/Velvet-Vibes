d77e06d86139b975e1c030c01cb578ff9baa44f8
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-02-11 13:00:19 +0530

    Docker config

--- api/db.js
+++ api/db.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const Mongo_URI = process.env.MONGOURL;
+
+console.log(Mongo_URI)
 async function connectToMongo() {
-  const result = await mongoose.connect(Mongo_URI);
-  if (result) {
-    console.log("Connected to Mongo succesfully");
-  } else {
-    console.log("Couldn't connect to Mongo succesfully");
+  try {
+    const result = await mongoose.connect(Mongo_URI);
+    if (result) {
+      console.log("Connected to Mongo succesfully");
+    } else {
+      console.log("Couldn't connect to Mongo succesfully");
+    }
+  } catch (error) {
+    console.log(error)
+
   }
 }
 
